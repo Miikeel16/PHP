9840ce685444ed5c193d87a74308785709b1e096
Miikeel16 <mikel.garcia06@somo.eus>
2025-01-15 22:56:49 +0100

    Delete PHP/EjerciciosSimples directory

--- PHP/EjerciciosSimples/Ejercicio1/Consulta.php
+++
@@ -1,9 +0,0 @@
-<?php
-        $numero = $_REQUEST["numero"];
-
-            if ($numero % 2 == 0) {
-                echo "El número $numero es par.";
-            } else {
-                echo "El número $numero es impar.";
-            }
-    ?>
--- PHP/EjerciciosSimples/Ejercicio1/Formulario.php
+++
@@ -1,7 +0,0 @@
-
-    <h1>¿Es par o impar?</h1>
-    <form action="Consulta.php">
-        <label for="numero">Introduce un número entero:</label>
-        <input type="number" name="numero" required>
-        <button type="submit">Verificar</button>
-    </form>
