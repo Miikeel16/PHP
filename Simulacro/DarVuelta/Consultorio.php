ed2b65cb1994487e6593d506baed215cb7fb5269
Miikeel16 <mikel.garcia06@somo.eus>
2025-01-25 15:58:46 +0100

    Delete Simulacro directory

--- Simulacro/DarVuelta/Consultorio.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $num1 = $_POST['num1'];
-    $num1_invertido = intval(strrev((string)$num1));
-    echo $num1_invertido;
-?>
--- Simulacro/Ejercicio2/Formulario.php
+++
@@ -1,7 +0,0 @@
-<form action="Consultorio.php" method="post">
-    <label for="num1">Primer numero: </label><br>
-    <input type="text" name="num1" required><br>
-    <label for="checkbox">Si esta activo suma impares, si no, suma pares</label><br>
-    <input type="checkbox" name="checkbox"><br>
-    <button type="sumbit">Enviar</button>
-</form>
