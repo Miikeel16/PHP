3e7e7eb373798b1f220362284289efea1068715f
Miikeel16 <mikel.garcia06@somo.eus>
2025-01-25 15:58:38 +0100

    Delete EjerciciosSimples directory

--- EjerciciosSimples/Ejercicio1/Consulta.php
+++
@@ -1,9 +0,0 @@
-<?php
-        $numero = $_POST["numero"];
-
-            if ($numero % 2 == 0) {
-                echo "El número $numero es par.";
-            } else {
-                echo "El número $numero es impar.";
-            }
-    ?>
